82bb823c8d9d1fca744121c1446a5e47e9f4038f
ponsianodeloor <ponsianodeloor@gmail.com>
2024-08-12 16:28:41 -0500

    configurar mf shopping con la ruta products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'shopping/products',
+    loadChildren: () => loadRemoteModule(
+      {
+        type: 'module',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './ProductsModule'
+      }
+    ).then(m => m.ProductsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
